59ffcfbf1c7017d693fbbf6da7e30992691273b9
Khusan <pypsikystlt@mail.ru>
2019-05-07 14:05:01 +0400

    7.05.2019

--- RegionL/urls.py
+++ RegionL/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from employees import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('cargostatus/', TemplateView.as_view(template_name='cargostatus.html')),
     path('admin/', admin.site.urls),
 ]
