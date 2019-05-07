aad5accc43538d2632b99411119a371786619db5
Khusan <pypsikystlt@mail.ru>
2019-05-07 15:01:57 +0400

    7.05.19

--- cargostatus/views.py
+++ cargostatus/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Cargostatus
 
+# получение данных из бд
+def index(request):
+    person = cargostatus.objects.all()
+    return render(request, "cargostatus.html", {"cargostatus": cargostatus})
+
+# сохранение данных в бд
+# def create(request):
+#     if request.method == "POST":
+#         tom = Person()
+#         tom.name = request.POST.get("name")
+#         tom.age = request.POST.get("age")
+#         tom.save()
+#     return HttpResponseRedirect("/")
 # Create your views here.
